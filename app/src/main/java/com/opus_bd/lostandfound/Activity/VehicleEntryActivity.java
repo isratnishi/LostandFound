e58a97ce7920a1759291433c3cf8b1ee6ccd8de7
suzauddaula <suzauddaula103@gmail.com>
2020-04-19 22:13:14 +0600

    vehiclenewdesign

---
+++ app/src/main/java/com/opus_bd/lostandfound/Activity/VehicleEntryActivity.java
@@ -0,0 +1,118 @@
+package com.opus_bd.lostandfound.Activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.opus_bd.lostandfound.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class VehicleEntryActivity extends AppCompatActivity {
+
+    boolean isllVehicleEntryChecked = true;
+    @BindView(R.id.llVehicleInfromation)
+    LinearLayout llVehicleInfromation;
+
+    @BindView(R.id.ivVehicleInformation)
+    ImageView ivVehicleInformation;
+
+    boolean isllVehicleIdentificationChecked = true;
+    @BindView(R.id.llVIdentityInfo)
+    LinearLayout llVIdentityInfo;
+
+    @BindView(R.id.ivVIdentityInfo)
+    ImageView ivVIdentityInfo;
+
+    boolean isllVPATChecked = true;
+    @BindView(R.id.llVPATInfo)
+    LinearLayout llVPATInfo;
+
+    @BindView(R.id.ivVPATInfo)
+    ImageView ivVPATInfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicle_entry);
+        ButterKnife.bind(this);
+        llVehicleInfromation.setVisibility(View.GONE);
+        llVIdentityInfo.setVisibility(View.GONE);
+        llVPATInfo.setVisibility(View.GONE);
+    }
+
+    @OnClick(R.id.ivVehicleInformation)
+    public void ivVehicleInformation(){
+        if (isllVehicleEntryChecked) {
+            // show password
+            llVehicleInfromation.setVisibility(View.VISIBLE);
+            Glide.with(this).load(R.drawable.ic_drop_up).into(ivVehicleInformation);
+            isllVehicleEntryChecked = false;
+        } else {
+            // hide password
+            llVehicleInfromation.setVisibility(View.GONE);
+            Glide.with(this).load(R.drawable.ic_drop_down).into(ivVehicleInformation);
+            isllVehicleEntryChecked = true;
+        }
+
+    }
+
+    @OnClick({R.id.ivVIdentityInfo,R.id.btnNext1})
+    public void ivVIdentityInfo(){
+
+        if (isllVehicleIdentificationChecked) {
+            // show password
+            llVehicleInfromation.setVisibility(View.GONE);
+            Glide.with(this).load(R.drawable.ic_drop_down).into(ivVehicleInformation);
+            isllVehicleEntryChecked = true;
+
+            llVIdentityInfo.setVisibility(View.VISIBLE);
+            Glide.with(this).load(R.drawable.ic_drop_up).into(ivVIdentityInfo);
+            isllVehicleIdentificationChecked = false;
+
+        } else {
+            // hide password
+            llVIdentityInfo.setVisibility(View.GONE);
+            Glide.with(this).load(R.drawable.ic_drop_down).into(ivVIdentityInfo);
+            isllVehicleIdentificationChecked = true;
+        }
+
+//        llVIdentityInfo.setVisibility(View.VISIBLE);
+//        llVehicleInfromation.setVisibility(View.GONE);
+//        llVPATInfo.setVisibility(View.GONE);
+    }
+
+
+
+    @OnClick({R.id.ivVPATInfo,R.id.btnNext2})
+    public void ivVPATInfo(){
+        if (isllVPATChecked) {
+            // show password
+            llVIdentityInfo.setVisibility(View.GONE);
+            Glide.with(this).load(R.drawable.ic_drop_up).into(ivVIdentityInfo);
+            isllVehicleIdentificationChecked = true;
+
+            llVIdentityInfo.setVisibility(View.VISIBLE);
+            Glide.with(this).load(R.drawable.ic_drop_up).into(ivVPATInfo);
+            isllVPATChecked = false;
+
+        } else {
+            // hide password
+            llVPATInfo.setVisibility(View.GONE);
+            Glide.with(this).load(R.drawable.ic_drop_down).into(ivVPATInfo);
+            isllVPATChecked = true;
+        }
+
+//        llVIdentityInfo.setVisibility(View.GONE);
+//        llVehicleInfromation.setVisibility(View.GONE);
+//        llVPATInfo.setVisibility(View.VISIBLE);
+
+    }
+
+}
