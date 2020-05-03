67e7e545d2bc9d28504e6cd6b93f615397c988b2
isratnishi <isratjahan27cse@gmail.com>
2020-05-03 11:32:33 +0600

    others field add

---
+++ app/src/main/java/com/opus_bd/lostandfound/Activity/OtherItem/CategoryListActivity.java
@@ -0,0 +1,26 @@
+package com.opus_bd.lostandfound.Activity.OtherItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.opus_bd.lostandfound.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class CategoryListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_list);
+        ButterKnife.bind(this);
+    }
+    @OnClick(R.id.llRoot)
+    public void llRoot(){
+        Intent intent = new Intent(this, ComputerActivity.class);
+startActivity(intent);
+    }
+}
